7820ae6cd1e198341c3a8f9de01babd4fba29805
sacarvy <sukhpreetben10@gmail.com>
2021-09-06 09:18:45 +0530

    Insitial commit

--- src/numbers/number.go
+++
@@ -1,8 +0,0 @@
-package numbers
-
-// Import declaration declares library packages referenced in this file.
-
-// "fmt" // A package in the Go standard library.
-// "os"  // OS functions like working with the file system
-
-var Num = "I am in Math"
